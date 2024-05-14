18ae95eda1529c81bd8aaf4b61b9156b9c54967f
avent0 <ezev77@gmail.com>
2024-05-14 11:47:03 +0100

    modif |logo|a propos|services|Contact iframe|

--- src/components/forms/ContactForm.vue
+++ src/components/forms/ContactForm.vue
@@ -4,7 +4,10 @@
             <div class="row">
                 <div class="col-lg-7">
                     <div id="map" style="height: 500px; overflow: hidden;">
-                        <img src="/public/assets/images/bus2.jpeg" alt="">
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3965.117759553162!2d2.41199257499168!3d6.378797093611543!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNsKwMjInNDMuNyJOIDLCsDI0JzUyLjQiRQ!5e0!3m2!1sfr!2srw!4v1715683460045!5m2!1sfr!2srw"
+                            width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade"></iframe>
                     </div>
                     <div class="row">
                         <div class="col-lg-6">
